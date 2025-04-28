a449f4ccccb1aed1ea5a7231126df934d48ff32b
Nat! <nat@mulle-kybernetik.com>
2025-04-28 18:17:06 +0200

    remove some warnings

--- src/http_parser.c
+++ src/http_parser.c
@@ -1814,7 +1814,7 @@ reexecute:
 
             case 2:
               parser->upgrade = 1;
-
+              __attribute__(( fallthrough)); // (nat) assume ?
             case 1:
               parser->flags |= F_SKIPBODY;
               break;
@@ -2384,6 +2384,7 @@ http_parser_parse_url(const char *buf, size_t buflen, int mode,
 
       case s_req_server_with_at:
         found_at = 1;
+        __attribute__(( fallthrough));
 
       /* FALLTROUGH */
       case s_req_server:
