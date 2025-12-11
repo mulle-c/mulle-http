a89745b1a9165e9e9017350a12838a939a97ed3c
Nat! <nat@mulle-kybernetik.com>
2025-12-11 22:04:39 +0100

    maintenance: update mulle-sde tooling

---
+++ test/include.h
@@ -0,0 +1,9 @@
+#ifndef mulle__http__test_include_h__
+#define mulle__http__test_include_h__
+
+// THIS FILE WILL BE CLOBBERED BY mulle-sde test craft
+
+#include <mulle-c11/mulle-c11.h>
+#include <mulle-http/mulle-http.h>
+
+#endif /*  */
